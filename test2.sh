97953fdf4c07bee6b648d307729488546e682439
HKumar6 <harishk9697@gmail.com>
2023-12-26 17:14:39 +0530

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#mkdir cloneRepos
+mkdir cloneRepos
 folder_to_cleanup="cloneRepos"
 
-if [ -d "$folder_to_cleanup" ]
-then
-    echo "cleaning up folder $folder_to_cleanup"
-    rm -rf "$folder_to_cleanup"
-else
-    echo "$folder_to_cleanup does not exist."
-fi
+#if [ -d "$folder_to_cleanup" ]
+#then
+#    echo "cleaning up folder $folder_to_cleanup"
+#    rm -rf "$folder_to_cleanup"
+#else
+#    echo "$folder_to_cleanup does not exist."
+#fi
 
 cd cloneRepos
 echo "in script file"
 #github_url="https://github.com/Harishk9697/robotframework-test-suite.git"
 #echo $github_url
-git clone https://github.com/Harishk9697/robotframework-test-suite.git /cloneRepos
+git clone https://github.com/Harishk9697/robotframework-test-suite.git
 #repo_basename=$(basename "$github_url")
 echo $HELLO
 ls
