9c4ab8e821aab5e3c37a916cf85f4941bac606b9
HKumar6 <harishk9697@gmail.com>
2023-12-26 16:52:51 +0530

    Update test2.sh file

--- test2.sh
+++ test2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir cloneRepos
+#mkdir cloneRepos
 folder_to_cleanup="cloneRepos"
 
 if [ -d "$folder_to_cleanup" ]
@@ -14,8 +14,16 @@ cd cloneRepos
 echo "in script file"
 #github_url="https://github.com/Harishk9697/robotframework-test-suite.git"
 #echo $github_url
-git clone https://github.com/Harishk9697/robotframework-test-suite.git
+git clone https://github.com/Harishk9697/robotframework-test-suite.git /cloneRepos
 #repo_basename=$(basename "$github_url")
 echo $HELLO
 ls
 robot --outputdir /report robotframework-test-suite/About.robot
+
+if [ $? -ne 0 ]; then
+    echo "Command robot execution failed"
+    aws s3 cp /report s3://tf-rf-scripts-spe-qaqc-bucket/RobotFrameworkReport/ && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
+else
+    echo "Command robot execution passed"
+    aws s3 cp /report s3://tf-rf-scripts-spe-qaqc-bucket/RobotFrameworkReport/ && echo "Copied report to s3 bucket" || echo "Copying report to s3 bucket failed"
+fi
