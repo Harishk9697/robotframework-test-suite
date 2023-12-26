45e2b19ef2a6df7ad3c9f49b15576db9199bd3dc
HKumar6 <harishk9697@gmail.com>
2023-12-26 16:47:03 +0530

    Update sh file

--- test2.sh
+++ test2.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 mkdir cloneRepos
+folder_to_cleanup="cloneRepos"
+
+if [ -d "$folder_to_cleanup" ]
+then
+    echo "cleaning up folder $folder_to_cleanup"
+    rm -rf "$folder_to_cleanup"
+else
+    echo "$folder_to_cleanup does not exist."
+fi
+
 cd cloneRepos
 echo "in script file"
-git clone https://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@github.com/iiakhtar/automation-aws
+#github_url="https://github.com/Harishk9697/robotframework-test-suite.git"
+#echo $github_url
+git clone https://github.com/Harishk9697/robotframework-test-suite.git
+#repo_basename=$(basename "$github_url")
 echo $HELLO
 ls
-robot --outputdir /tmp automation-aws/About.robot
+robot --outputdir /report robotframework-test-suite/About.robot
